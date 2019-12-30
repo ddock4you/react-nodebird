a77c5ec2e60ce2c3b99e5e4d5a1dff7a552f9bca
ddock4you <ddock4you@gmail.com>
2019-12-30 17:51:20 +0900

    ant design 적용 및 children props 이해

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
+import AppLayout from "../components/AppLayout";
 
 const Home = () => {
     return (
         <>
-            <Link href="/about">
-                <a>about</a>
-            </Link>
-            <div>Hello, Next</div>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.25.3/antd.css"
+                ></link>
+            </Head>
+            <AppLayout>
+                <Link href="/about">
+                    <a>about</a>
+                </Link>
+                <div>Hello, Next</div>
+            </AppLayout>
         </>
     );
 };
