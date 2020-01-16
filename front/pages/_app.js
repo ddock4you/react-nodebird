719b0a92b08bb624d998f7d0030f0f4633665060
ddock4you <ddock4you@gmail.com>
2020-01-16 16:25:02 +0900

    - redux-sage 연결

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -6,6 +6,8 @@ import withRedux from "next-redux-wrapper";
 import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducer from "../reducers";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "../sagas";
 
 const Nodebird = ({ Component, store }) => {
     return (
@@ -31,15 +33,20 @@ Nodebird.propTypes = {
 
 // 고차 컴포넌트
 export default withRedux((initialState, options) => {
-    const middlewares = [];
-    const enhancer = compose(
-        // middleware끼리 합성
-        applyMiddleware(...middlewares),
-        !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined" // chrome redux devtools 사용 코드
-            ? window.__REDUX_DEVTOOLS_EXTENSION__()
-            : f => f
-    );
+    const sagaMiddleware = createSagaMiddleware();
+    const middlewares = [sagaMiddleware];
+    const enhancer =
+        process.env.NODE_ENV === "production"
+            ? compose(applyMiddleware(...middlewares))
+            : compose(
+                  // middleware끼리 합성
+                  applyMiddleware(...middlewares),
+                  !options.isServer &&
+                      window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined" // chrome redux devtools 사용 코드
+                      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+                      : f => f
+              );
     const store = createStore(reducer, initialState, enhancer);
-
+    sagaMiddleware.run(rootSaga);
     return store;
 })(Nodebird);
