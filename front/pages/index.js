183a9427e69c8fcd093dbb67cf08deba0d98007d
ddock4you <ddock4you@gmail.com>
2020-01-17 16:22:31 +0900

    - redux-saga에서 제너레이터 작동 원리 이해

--- front/pages/index.js
+++ front/pages/index.js
@@ -2,16 +2,24 @@ import React, { useEffect } from "react";
 import PostForm from "../components/PostForm";
 import PostCard from "../components/PostCard";
 import { useDispatch, useSelector } from "react-redux";
-import { loginAction, logoutAction } from "../reducers/user";
 
 const Home = () => {
     const dispatch = useDispatch();
     const { user, isLoggedIn } = useSelector(state => state.user);
     const { mainPosts } = useSelector(state => state.post);
 
-    // useEffect(() => {
-    //     dispatch(loginAction);
-    // }, []);
+    useEffect(() => {
+        dispatch({
+            type: "HELLO_SAGA"
+        });
+        dispatch({
+            type: "HELLO_SAGA"
+        });
+        dispatch({
+            type: "HELLO_SAGA"
+        });
+    }, []);
+
     return (
         <div>
             {user ? (
--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,16 +1,23 @@
-import { all } from "redux-saga/effects";
+import { all, fork, takeLatest, put, take, delay } from "redux-saga/effects";
 import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from "../reducers/user";
 
-function loginAPI() {}
+const HELLO_SAGA = "HELLO_SAGA";
+
+function loginAPI() {
+    // 서버에 요청을 보내는 부분
+}
 
 function* login() {
     try {
-        yield call(loginAPI);
+        // yield call(loginAPI);
+        yield delay(100);
         yield put({
+            // put은 dispatch 동일
             type: LOG_IN_SUCCESS
         });
     } catch (e) {
-        console.erroe(e);
+        // loginAPI 실패
+        console.error(e);
         yield put({
             type: LOG_IN_FAILURE
         });
@@ -21,6 +28,14 @@ function* watchLogin() {
     yield takeLatest(LOG_IN, login);
 }
 
+function* helloSaga() {
+    console.log("before saga");
+    while (true) {
+        yield take(HELLO_SAGA);
+        console.log("hello saga");
+    }
+}
+
 export default function* userSaga() {
-    yield all([fork(watchLogin)]);
+    yield helloSaga();
 }
