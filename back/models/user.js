9dd3eff4f8bc22acb692e3cc3af38cb2fc58d891
ddock4you <ddock4you@gmail.com>
2020-01-29 17:53:30 +0900

    - sequlize를 사용하여 db 작성

--- back/models/user.js
+++ back/models/user.js
@@ -24,8 +24,11 @@ module.exports = (sequelize, DataTypes) => {
 
     // 테이블 관계를 입력
     User.associate = db => {
-        db.User.hasMany(db.Post);
+        db.User.hasMany(db.Post, { as: "Posts" });
         db.User.hasMany(db.Comment);
+        db.User.belongsToMany(db.Post, { through: "Like", as: "Liked" });
+        db.User.belongsToMany(db.User, { through: "Follow", as: "Followers" });
+        db.User.belongsToMany(db.User, { through: "Follow", as: "Followings" });
     };
 
     return User;
