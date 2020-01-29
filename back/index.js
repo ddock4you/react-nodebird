3351eedfb76d12f7a81a8c861607566ea63db9ab
ddock4you <ddock4you@gmail.com>
2020-01-29 18:36:00 +0900

    - 회원가입 컨트롤러 추가

--- back/index.js
+++ back/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 
 const db = require("./models");
-const userAPIRouter = require("./route/user");
-const postAPIRouter = require("./route/post");
-const postsAPIRouter = require("./route/posts");
+const userAPIRouter = require("./routes/user");
+const postAPIRouter = require("./routes/post");
+const postsAPIRouter = require("./routes/posts");
 
 const app = express();
 db.sequelize.sync();
+
+// 아래 2개를 해야 req.body가 작동함
+app.use(express.json()); // json 형식의 본문을 처리
+app.use(express.urlencoded({ extended: true })); // form으로 넘어온 데이터를 처리
 
 // 요청에 대한 응답을 보내주는 영역(?) -> controller
 // API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,9 +1,37 @@
-const express = require("exporess");
+const express = require("express");
+const bcrypt = require("bcrypt");
+const db = require("../models");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {});
 
-router.post("/", (req, res) => {});
+router.post("/", async (req, res, next) => {
+    // POST  /api/user 회원가입
+    try {
+        const exUser = await db.User.findOne({
+            where: {
+                userId: req.body.userId
+            }
+        });
+        if (exUser) {
+            return res.status(400).send("이미 사용 중인 아이디입니다.");
+        }
+        const hashedPassword = await bcrypt.hash(req.body.password, 12);
+        const newUser = await db.User.create({
+            nickname: req.body.nickname,
+            userId: req.bopdy.userId,
+            password: hashedPassword
+        });
+        console.log(newUser);
+        return res.status(200).json(newUser);
+    } catch (e) {
+        console.error(e);
+        // return res.status(403).send(e);
+        // 에러 처리를 여기서
+        return next(e);
+    }
+});
 
 router.get("api/user/:id", (req, res) => {
     // 남의 정보 가져오는 것
