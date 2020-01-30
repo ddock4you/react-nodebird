c7bee95c14528c5ab623c946e1bc9b222668f3a5
ddock4you <ddock4you@gmail.com>
2020-01-30 15:31:39 +0900

    - 로그인 설정, dotenv 설정

--- back/index.js
+++ back/index.js
@@ -1,24 +1,36 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const expressSassion = require("express-session");
+const dotenv = require("dotenv");
 
 const db = require("./models");
 const userAPIRouter = require("./routes/user");
 const postAPIRouter = require("./routes/post");
 const postsAPIRouter = require("./routes/posts");
 
+dotenv.config();
 const app = express();
 db.sequelize.sync();
 
 app.use(morgan("dev"));
-// 아래 2개를 해야 req.body가 작동함
 app.use(express.json()); // json 형식의 본문을 처리
 app.use(express.urlencoded({ extended: true })); // form으로 넘어온 데이터를 처리
-
 app.use(cors());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+    expressSassion({
+        resave: false, // 매번 세션 강제 저장
+        saveUnitialized: false, // 빈 값도 저장
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+            httpOnly: true, // 자바스크립트로 cookie에 접근할 수 없다.
+            secure: false // https를 쓸 때 true
+        }
+    })
+);
 
-// 요청에 대한 응답을 보내주는 영역(?) -> controller
-// API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
 app.use("/api/user", userAPIRouter);
 app.use("/api/post", postAPIRouter);
 app.use("/api/posts", postsAPIRouter);
