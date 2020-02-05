b63a64d26f6a6ee9eb835e814d83775796c3772f
ddock4you <ddock4you@gmail.com>
2020-02-05 17:42:28 +0900

    - react-slick으로 이미지 슬라이더 기능 추가

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import PropTypes from "prop-types";
 import { Icon } from "antd";
+import ImagesZoom from "./ImagesZoom";
 
 const PostImages = ({ images }) => {
+    const [showImagesZoom, setShowImagesZoom] = useState(false);
+    const onZoom = useCallback(() => {
+        setShowImagesZoom(true);
+    }, []);
+
+    const onClose = useCallback(() => {
+        setShowImagesZoom(false);
+    }, []);
+
     if (images.length === 1) {
-        return <img src={`http://localhost:3065/${images[0].src}`} />;
+        return (
+            <>
+                <img
+                    src={`http://localhost:3065/${images[0].src}`}
+                    onClick={onZoom}
+                />
+                {showImagesZoom && (
+                    <ImagesZoom images={images} onClose={onClose} />
+                )}
+            </>
+        );
     }
     if (images.length === 2) {
         return (
@@ -12,17 +32,29 @@ const PostImages = ({ images }) => {
                 <img
                     src={`http://localhost:3065/${images[0].src}`}
                     width="50%"
+                    onClick={onZoom}
+                    onClose={onClose}
                 />
                 <img
                     src={`http://localhost:3065/${images[1].src}`}
                     width="50%"
+                    onClick={onZoom}
+                    onClose={onClose}
                 />
+                {showImagesZoom && (
+                    <ImagesZoom images={images} onClose={onClose} />
+                )}
             </div>
         );
     }
     return (
         <div>
-            <img src={`http://localhost:3065/${images[0].src}`} width="50%" />
+            <img
+                src={`http://localhost:3065/${images[0].src}`}
+                width="50%"
+                onClick={onZoom}
+                onClose={onClose}
+            />
             <div
                 style={{
                     display: "inline-block",
@@ -30,11 +62,13 @@ const PostImages = ({ images }) => {
                     textAlign: "center",
                     verticalAlign: "middle"
                 }}
+                onClick={onZoom}
             >
                 <Icon type="plus" />
                 <br />
                 {images.length - 1} 개의 사진 더보기
             </div>
+            {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
         </div>
     );
 };
--- front/pages/_app.js
+++ front/pages/_app.js
@@ -20,6 +20,17 @@ const Nodebird = ({ Component, store, pageProps }) => {
                     rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.25.3/antd.css"
                 />
+                <link
+                    rel="stylesheet"
+                    type="text/css"
+                    charset="UTF-8"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+                />
+                <link
+                    rel="stylesheet"
+                    type="text/css"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+                />
             </Head>
             <AppLayout>
                 <Component {...pageProps} />
