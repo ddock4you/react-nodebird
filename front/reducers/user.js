64684aee87f1dfa48b75365547d2e4df66aa8a51
ddock4you <ddock4you@gmail.com>
2020-02-07 21:04:20 +0900

    - 프로필 화면에서 닉네임 변경 기능 추가

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -9,7 +9,9 @@ export const initialState = {
     me: null, // 내 정보
     followingList: [], // 팔로잉 리스트
     followerList: [], // 팔로워 리스트
-    userInfo: null // 남의 정보
+    userInfo: null, // 남의 정보
+    isEditingNickname: false, // 이름 변경 중
+    editNicknameErrorReason: "" // 이름 변경 실패 사유
 };
 //
 export const SIGN_UP_REQUEST = "SIGN_UP_REQUEST";
@@ -51,6 +53,10 @@ export const UNFOLLOW_USER_FAILURE = "UNFOLLOW_USER_FAILURE";
 export const REMOVE_FOLLOWER_REQUEST = "REMOVE_FOLLOWER_REQUEST";
 export const REMOVE_FOLLOWER_SUCCESS = "REMOVE_FOLLOWER_SUCCESS";
 export const REMOVE_FOLLOWER_FAILURE = "REMOVE_FOLLOWER_FAILURE";
+
+export const EDIT_NICKNAME_REQUEST = "EDIT_NICKNAME_REQUEST";
+export const EDIT_NICKNAME_SUCCESS = "EDIT_NICKNAME_SUCCESS";
+export const EDIT_NICKNAME_FAILURE = "EDIT_NICKNAME_FAILURE";
 
 export const ADD_POST_TO_ME = "ADD_POST_TO_ME";
 
@@ -258,6 +264,30 @@ export default (state = initialState, action) => {
         case REMOVE_FOLLOWER_FAILURE: {
             return {
                 ...state
+            };
+        }
+        case EDIT_NICKNAME_REQUEST: {
+            return {
+                ...state,
+                isEditingNickname: true,
+                editNicknameErrorReason: ""
+            };
+        }
+        case EDIT_NICKNAME_SUCCESS: {
+            return {
+                ...state,
+                isEditingNickname: false,
+                me: {
+                    ...state.me,
+                    nickname: action.data
+                }
+            };
+        }
+        case EDIT_NICKNAME_FAILURE: {
+            return {
+                ...state,
+                isEditingNickname: false,
+                editNicknameErrorReason: action.error
             };
         }
         default: {
