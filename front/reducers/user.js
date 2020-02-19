71000d5e933cc69ef777d7c802f2899081007477
ddock4you <ddock4you@gmail.com>
2020-02-19 15:40:27 +0900

    - user redux까지 immer처리 완료

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,3 +1,5 @@
+import produce from "immer";
+
 // store, 초기값
 export const initialState = {
     isLoggedIn: false, // 로그인 여부
@@ -13,7 +15,7 @@ export const initialState = {
     isEditingNickname: false, // 이름 변경 중
     editNicknameErrorReason: "", // 이름 변경 실패 사유
     hasMoreFollower: false,
-    hasMoreFollowing: false, 
+    hasMoreFollowing: false
 };
 //
 export const SIGN_UP_REQUEST = "SIGN_UP_REQUEST";
@@ -71,242 +73,344 @@ export const signUpRequestAction = data => {
     };
 };
 
-
 // 리듀서 -> Action의 결과로 state를 어떻게 바꿀지 정의
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case LOG_IN_REQUEST: {
-            return {
-                ...state,
-                isLoggedIn: true,
-                logInErrorReason: ""
-            };
-        }
-        case LOG_IN_SUCCESS: {
-            return {
-                ...state,
-                isLoggedIn: false,
-                me: action.data,
-                isLoading: false
-            };
-        }
-        case LOG_IN_FAILURE: {
-            return {
-                ...state,
-                isLoggedIn: false,
-                logInErrorReason: action.error,
-                me: null
-            };
-        }
-        case LOG_OUT_REQUEST: {
-            return {
-                ...state,
-                isLoggingOut: true
-            };
-        }
-        case LOG_OUT_SUCCESS: {
-            return {
-                ...state,
-                isLoggingOut: false,
-                me: null
-            };
-        }
-        case SIGN_UP_REQUEST: {
-            return {
-                ...state,
-                isSigningUp: true,
-                isSignedUp: false,
-                signUpErrorReason: ""
-            };
-        }
-        case SIGN_UP_SUCCESS: {
-            return {
-                ...state,
-                isSigningUp: false,
-                isSignedUp: true
-            };
-        }
-        case SIGN_UP_FAILURE: {
-            return {
-                ...state,
-                isSigningUp: false,
-                signUpErrorReason: action.error
-            };
-        }
-        case LOAD_USER_REQUEST: {
-            return {
-                ...state
-            };
-        }
-        case LOAD_USER_SUCCESS: {
-            if (action.me) {
-                return {
-                    ...state,
-                    me: action.data
-                };
+    return produce(state, draft => {
+        switch (action.type) {
+            case LOG_IN_REQUEST: {
+                draft.isLoggedIn = true;
+                draft.logInErrorReason = "";
+                break;
+                // return {
+                //     ...state,
+                //     isLoggedIn: true,
+                //     logInErrorReason: ""
+                // };
             }
-            return {
-                ...state,
-                userInfo: action.data
-            };
-        }
-        case LOAD_USER_FAILURE: {
-            return {
-                ...state
-            };
-        }
-        case FOLLOW_USER_REQUEST: {
-            return {
-                ...state
-            };
-        }
-        case FOLLOW_USER_SUCCESS: {
-            return {
-                ...state,
-                me: {
-                    ...state.me,
-                    Followings: [{ id: action.data }, ...state.me.Followings]
+            case LOG_IN_SUCCESS: {
+                draft.isLoggedIn = false;
+                draft.me = action.data;
+                draft.isLoading = false;
+                break;
+                // return {
+                //     ...state,
+                //     isLoggedIn: false,
+                //     me: action.data,
+                //     isLoading: false
+                // };
+            }
+            case LOG_IN_FAILURE: {
+                draft.isLoggedIn = false;
+                draft.logInErrorReason = action.error;
+                draft.me = null;
+                break;
+                // return {
+                //     ...state,
+                //     isLoggedIn: false,
+                //     logInErrorReason: action.error,
+                //     me: null
+                // };
+            }
+            case LOG_OUT_REQUEST: {
+                draft.isLoggingOut = true;
+                break;
+                // return {
+                //     ...state,
+                //     isLoggingOut: true
+                // };
+            }
+            case LOG_OUT_SUCCESS: {
+                draft.isLoggingOut = false;
+                draft.me = null;
+                break;
+                // return {
+                //     ...state,
+                //     isLoggingOut: false,
+                //     me: null
+                // };
+            }
+            case SIGN_UP_REQUEST: {
+                draft.isSigningUp = true;
+                draft.isSignUp = false;
+                draft.signUpErrorReason = "";
+                break;
+                // return {
+                //     ...state,
+                //     isSigningUp: true,
+                //     isSignedUp: false,
+                //     signUpErrorReason: ""
+                // };
+            }
+            case SIGN_UP_SUCCESS: {
+                draft.isSigninUp = false;
+                draft.isSigndUp = true;
+                break;
+                // return {
+                //     ...state,
+                //     isSigningUp: false,
+                //     isSignedUp: true
+                // };
+            }
+            case SIGN_UP_FAILURE: {
+                draft.isSigningUp = false;
+                draft.signUpErrorReason = action.error;
+                break;
+                // return {
+                //     ...state,
+                //     isSigningUp: false,
+                //     signUpErrorReason: action.error
+                // };
+            }
+            case LOAD_USER_REQUEST: {
+                break;
+                // return {
+                //     ...state
+                // };
+            }
+            case LOAD_USER_SUCCESS: {
+                if (action.me) {
+                    draft.me = action.data;
+                    break;
+                    // return {
+                    //     ...state,
+                    //     me: action.data
+                    // };
                 }
-            };
+                draft.userinfo = action.data;
+                break;
+                // return {
+                //     ...state,
+                //     userInfo: action.data
+                // };
+            }
+            case LOAD_USER_FAILURE: {
+                break;
+                // return {
+                //     ...state
+                // };
+            }
+            case FOLLOW_USER_REQUEST: {
+                break;
+                // return {
+                //     ...state
+                // };
+            }
+            case FOLLOW_USER_SUCCESS: {
+                draft.me.Followings.unshift({ id: action.data });
+                break;
+                // return {
+                //     ...state,
+                //     me: {
+                //         ...state.me,
+                //         Followings: [
+                //             { id: action.data },
+                //             ...state.me.Followings
+                //         ]
+                //     }
+                // };
+            }
+            case FOLLOW_USER_FAILURE: {
+                break;
+                // return {
+                //     ...state
+                // };
+            }
+            case UNFOLLOW_USER_REQUEST: {
+                break;
+                // return {
+                //     ...state
+                // };
+            }
+            case UNFOLLOW_USER_SUCCESS: {
+                const index = draft.me.Followings.findIndex(
+                    v => v.id === action.data
+                );
+                draft.me.Followings.splice(index, 1);
+                const index2 = draft.me.Followings.findIndex(
+                    v => v.id === action.data
+                );
+                draft.me.Followings.splice(index2, 1);
+                break;
+                // return {
+                //     ...state,
+                //     me: {
+                //         ...state.me,
+                //         Followings: state.me.Followings.filter(
+                //             v => v.id !== action.data
+                //         )
+                //     },
+                //     followingList: state.followingList.filter(
+                //         v => v.id !== action.data
+                //     )
+                // };
+            }
+            case UNFOLLOW_USER_FAILURE: {
+                break;
+                // return {
+                //     ...state
+                // };
+            }
+            case ADD_POST_TO_ME: {
+                draft.me.Posts.unshift({ id: action.data });
+                break;
+                // return {
+                //     ...state,
+                //     me: {
+                //         ...state.me,
+                //         Posts: [{ id: action.data }, ...state.me.Posts]
+                //     }
+                // };
+            }
+            case REMOVE_POST_TO_ME: {
+                draft.me.Posts.unshift({ id: action.data });
+                break;
+                // return {
+                //     ...state,
+                //     me: {
+                //         ...state.me,
+                //         Posts: state.me.Posts.filter(v => v.id !== action.data)
+                //     }
+                // };
+            }
+            case LOAD_FOLLOWERS_REQUEST: {
+                draft.followerList = !action.offset ? [] : draft.followerList;
+                draft.hasMoreFollower = action.offset
+                    ? state.hasMoreFollower
+                    : true;
+                break;
+                // return {
+                //     ...state,
+                //     // 처음 데이터를 가져올 때는 더보기 버튼을 true로
+                //     hasMoreFollower: action.offset
+                //         ? state.hasMoreFollower
+                //         : true
+                // };
+            }
+            case LOAD_FOLLOWERS_SUCCESS: {
+                action.data.forEach(d => {
+                    draft.followerList.push(d);
+                });
+                draft.hasMoreFollower = action.data.length === 3;
+                break;
+                // return {
+                //     ...state,
+                //     followerList: state.followerList.concat(action.data),
+                //     hasMoreFollower: action.data.length === 3
+                // };
+            }
+            case LOAD_FOLLOWERS_FAILURE: {
+                break;
+                // return {
+                //     ...state
+                // };
+            }
+            case LOAD_FOLLOWINGS_REQUEST: {
+                draft.followingList = !action.offset ? [] : draft.followingList;
+                draft.hasMoreFollower = action.offset
+                    ? draft.hasMoreFollowing
+                    : true;
+                break;
+                // return {
+                //     ...state,
+                //     hasMoreFollowing: action.offset
+                //         ? state.hasMoreFollowing
+                //         : true
+                // };
+            }
+            case LOAD_FOLLOWINGS_SUCCESS: {
+                action.data.forEach(d => {
+                    draft.followingList.push(d);
+                });
+                draft.hasMoreFollowing = action.data.length === 3;
+                break;
+                // return {
+                //     ...state,
+                //     followingList: state.followingList.concat(action.data),
+                //     hasMoreFollowing: action.data.length === 3
+                // };
+            }
+            case LOAD_FOLLOWINGS_FAILURE: {
+                break;
+                // return {
+                //     ...state
+                // };
+            }
+            case REMOVE_FOLLOWER_REQUEST: {
+                break;
+                // return {
+                //     ...state
+                // };
+            }
+            case REMOVE_FOLLOWER_SUCCESS: {
+                const index = draft.me.Followers.findIndex(
+                    v => v.id === action.data
+                );
+                draft.me.Followers.splice(index, 1);
+                const index2 = draft.me.FollowerList.findIndex(
+                    v => v.id === action.data
+                );
+                draft.me.FollowerList.splice(index2, 1);
+                break;
+                // return {
+                //     ...state,
+                //     me: {
+                //         ...state.me,
+                //         Followers: state.me.Followers.filter(
+                //             v => v.id !== action.data
+                //         )
+                //     },
+                //     followerList: state.followerList.filter(
+                //         v => v.id !== action.data
+                //     )
+                // };
+            }
+            case REMOVE_FOLLOWER_FAILURE: {
+                break;
+                // return {
+                //     ...state
+                // };
+            }
+            case EDIT_NICKNAME_REQUEST: {
+                draft.isEditingNickname = true;
+                draft.editNicknameErrorReason = "";
+                break;
+                // return {
+                //     ...state,
+                //     isEditingNickname: true,
+                //     editNicknameErrorReason: ""
+                // };
+            }
+            case EDIT_NICKNAME_SUCCESS: {
+                draft.isEditingNickname = false;
+                draft.me.nickname = action.data;
+                break;
+                // return {
+                //     ...state,
+                //     isEditingNickname: false,
+                //     me: {
+                //         ...state.me,
+                //         nickname: action.data
+                //     }
+                // };
+            }
+            case EDIT_NICKNAME_FAILURE: {
+                draft.isEditingNickname = false;
+                draft.editNicknameErrorReason = action.error;
+                break;
+                // return {
+                //     ...state,
+                //     isEditingNickname: false,
+                //     editNicknameErrorReason: action.error
+                // };
+            }
+            default: {
+                break;
+                // return {
+                //     ...state
+                // };
+            }
         }
-        case FOLLOW_USER_FAILURE: {
-            return {
-                ...state
-            };
-        }
-        case UNFOLLOW_USER_REQUEST: {
-            return {
-                ...state
-            };
-        }
-        case UNFOLLOW_USER_SUCCESS: {
-            return {
-                ...state,
-                me: {
-                    ...state.me,
-                    Followings: state.me.Followings.filter(
-                        v => v.id !== action.data
-                    )
-                },
-                followingList: state.followingList.filter(
-                    v => v.id !== action.data
-                )
-            };
-        }
-        case UNFOLLOW_USER_FAILURE: {
-            return {
-                ...state
-            };
-        }
-        case ADD_POST_TO_ME: {
-            return {
-                ...state,
-                me: {
-                    ...state.me,
-                    Posts: [{ id: action.data }, ...state.me.Posts]
-                }
-            };
-        }
-        case REMOVE_POST_TO_ME: {
-            return {
-                ...state,
-                me: {
-                    ...state.me,
-                    Posts: state.me.Posts.filter(v=> v.id !==action.data)
-                }
-            };
-        }
-        case LOAD_FOLLOWERS_REQUEST: {
-            return {
-                ...state,
-                // 처음 데이터를 가져올 때는 더보기 버튼을 true로
-                hasMoreFollower: action.offset ? state.hasMoreFollower : true
-            };
-        }
-        case LOAD_FOLLOWERS_SUCCESS: {
-            return {
-                ...state,
-                followerList: state.followerList.concat(action.data),
-                hasMoreFollower: action.data.length === 3
-            };
-        }
-        case LOAD_FOLLOWERS_FAILURE: {
-            return {
-                ...state
-            };
-        }
-        case LOAD_FOLLOWINGS_REQUEST: {
-            return {
-                ...state,
-                hasMoreFollowing: action.offset ? state.hasMoreFollowing : true
-            };
-        }
-        case LOAD_FOLLOWINGS_SUCCESS: {
-            return {
-                ...state,
-                followingList: state.followingList.concat(action.data),
-                hasMoreFollowing: action.data.length === 3
-            };
-        }
-        case LOAD_FOLLOWINGS_FAILURE: {
-            return {
-                ...state
-            };
-        }
-        case REMOVE_FOLLOWER_REQUEST: {
-            return {
-                ...state
-            };
-        }
-        case REMOVE_FOLLOWER_SUCCESS: {
-            return {
-                ...state,
-                me: {
-                    ...state.me,
-                    Followers: state.me.Followers.filter(
-                        v => v.id !== action.data
-                    )
-                },
-                followerList: state.followerList.filter(
-                    v => v.id !== action.data
-                )
-            };
-        }
-        case REMOVE_FOLLOWER_FAILURE: {
-            return {
-                ...state
-            };
-        }
-        case EDIT_NICKNAME_REQUEST: {
-            return {
-                ...state,
-                isEditingNickname: true,
-                editNicknameErrorReason: ""
-            };
-        }
-        case EDIT_NICKNAME_SUCCESS: {
-            return {
-                ...state,
-                isEditingNickname: false,
-                me: {
-                    ...state.me,
-                    nickname: action.data
-                }
-            };
-        }
-        case EDIT_NICKNAME_FAILURE: {
-            return {
-                ...state,
-                isEditingNickname: false,
-                editNicknameErrorReason: action.error
-            };
-        }
-        default: {
-            return {
-                ...state
-            };
-        }
-    }
+    });
 };
 
 // export default reducer;
