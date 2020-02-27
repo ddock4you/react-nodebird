a5051c91aa1751949afec0cb34ddd0c3b0099ce0
ddock4you <ddock4you@gmail.com>
2020-02-27 15:10:44 +0900

    - 프론트 config.js 다시 수정

--- front/config/config.js
+++ front/config/config.js
@@ -1,7 +1,7 @@
-export const backUrl =
-    process.env.NODE_ENV === "production"
-        ? "http://15.165.196.57"
-        : "http://localhost:3065";
+// export const backUrl =
+//     process.env.NODE_ENV === "production"
+//         ? "http://15.165.196.57"
+//         : "http://localhost:3065";
 
 export const frontUrl = (process.env.NODE_ENV = "production"
     ? "http://15.165.132.128"
